803d11ab07a1c7d0114a54bbbebfd9de723cfc7e
knightkoo <wesam.dan@gmail.com>
2019-11-02 01:50:17 +0900

    project create and CameraAgent Apply

---
+++ Assets/Scripts/CameraAgent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAgent : MonoBehaviour
+{
+    [SerializeField] Camera m_TargetCamera;
+    [SerializeField] float m_BoardUnit;
+
+    void Start()
+    {
+        m_TargetCamera.orthographicSize = m_BoardUnit / m_TargetCamera.aspect;
+    }
+}
